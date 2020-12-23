db9e4fc15e4069338378887bb90eaf8e4ef8e858
Victor Nastasa <victornast@gmail.com>
2020-12-23 13:10:39 +0100

    iteration 3

--- app.js
+++ app.js
@@ -25,8 +25,33 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
-
 // Our routes go here:
+
+app.get('/', (request, response) => {
+  response.render('home', { pageTitle: 'Home' });
+});
+
+app.get('/artist-search', (request, response) => {
+  const searchQuery = request.query.q;
+  spotifyApi
+    .searchArtists(searchQuery)
+    .then((data) => {
+      console.log('The received data from the API: ', data.body);
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      console.log(data.body.artists.items[0]);
+      response.render('artist-search-results', {
+        pageTitle: 'Search Results',
+        artistList: data.body.artists
+      });
+    })
+    .catch((err) =>
+      console.log('The error while searching artists occurred: ', err)
+    );
+});
+
+app.get('*', (request, response) => {
+  response.render('error', { pageTitle: '404' });
+});
 
 app.listen(3000, () =>
   console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')

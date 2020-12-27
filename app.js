da0194e29656f4b3184293cca644759870ba48d6
Victor Nastasa <victornast@gmail.com>
2020-12-27 11:22:31 +0100

    iteration 5

--- app.js
+++ app.js
@@ -36,9 +36,9 @@ app.get('/artist-search', (request, response) => {
   spotifyApi
     .searchArtists(searchQuery)
     .then((data) => {
-      console.log('The received data from the API: ', data.body);
+      // console.log('The received data from the API: ', data.body);
       // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
-      console.log(data.body.artists.items[0]);
+      // console.log(data.body.artists.items[0]);
       response.render('artist-search-results', {
         pageTitle: 'Search Results',
         artistList: data.body.artists
@@ -46,6 +46,38 @@ app.get('/artist-search', (request, response) => {
     })
     .catch((err) =>
       console.log('The error while searching artists occurred: ', err)
+    );
+});
+
+app.get('/albums/:artistId', (request, response) => {
+  const artistId = request.params.artistId;
+  spotifyApi
+    .getArtistAlbums(artistId)
+    .then((data) => {
+      // console.log('Albums from API:', data.body);
+      response.render('albums', {
+        pageTitle: `Albums from ${data.body.items[0].artists[0].name}`,
+        artistAlbums: data.body
+      });
+    })
+    .catch((err) =>
+      console.log('The error while searching albums occured: ', err)
+    );
+});
+
+app.get('/tracks/:albumId', (request, response) => {
+  const albumId = request.params.albumId;
+  spotifyApi
+    .getAlbumTracks(albumId, { limit: 5, offset: 1 })
+    .then((data) => {
+      console.log('Tracks: ', data.body);
+      response.render('tracks', {
+        pageTitle: 'Tracks',
+        albumTracks: data.body
+      });
+    })
+    .catch((err) =>
+      console.log('The error while searching for tracks occured: ', err)
     );
 });
 
